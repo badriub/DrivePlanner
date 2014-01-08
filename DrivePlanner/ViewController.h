829b2f66923ab5bca00f11244598a6afe7e06d7a
Badari Madhav <badari@Badaris-MacBook-Air.local>
2014-01-07 19:42:26 -0500

    Initial Commit

---
+++ DrivePlanner/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DrivePlanner
+//
+//  Created by Badari Madhav on 1/7/14.
+//  Copyright (c) 2014 org.poobad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ DrivePlannerTests/DrivePlannerTests.m
@@ -0,0 +1,34 @@
+//
+//  DrivePlannerTests.m
+//  DrivePlannerTests
+//
+//  Created by Badari Madhav on 1/7/14.
+//  Copyright (c) 2014 org.poobad. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DrivePlannerTests : XCTestCase
+
+@end
+
+@implementation DrivePlannerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
